a4cbea977fa2922b244453c761f20952991a370e
ApacheAA <apache.gtao@gmail.com>
2019-06-30 10:48:02 +0300

    PS script execution policy fixed

--- download.ps1
+++ download.ps1
@@ -1,8 +1,8 @@
 #Download PSTools from Microsoft repository
 (new-object System.Net.WebClient).DownloadFile('https://download.sysinternals.com/files/PSTools.zip',$env:USERPROFILE+'\tmp.zip')
-echo File tmp.zip downloaded to $env:USERPROFILE
+echo "File tmp.zip downloaded to $env:USERPROFILE"
 #Unzip archive
-Expand-Archive -Path $env:USERPROFILE+'\tmp.zip' -Destination $env:USERPROFILE'\PSTools' -Force
+Expand-Archive -Path $env:USERPROFILE\tmp.zip -Destination $env:USERPROFILE\PSTools -Force
 #Delete archive
-erase tmp.zip
-echo "PSTools succesfully installed in $env:USERPROFILE'\PSTools'"
+erase $env:USERPROFILE\tmp.zip
+echo "PSTools succesfully installed in $env:USERPROFILE\PSTools"
